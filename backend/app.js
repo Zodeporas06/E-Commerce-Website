a219eb20d1cd4a9dabbd8b536b927fe42c1d979b
Zodeporas06 <zodeporas06@gmail.com>
2023-02-27 22:29:58 +0530

    minor changes

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const app = express();
 
 //app.use(bodyParser.urlencoded({extended: false}));
+
+//To get the data in json file for the database
 app.use(express.json());
 
 const product = require("./routes/productRoute.js");
