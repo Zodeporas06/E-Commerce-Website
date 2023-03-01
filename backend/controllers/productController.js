896892532c8705c635543a639e6420095df2e637
Zodeporas06 <zodeporas06@gmail.com>
2023-03-01 23:21:23 +0530

    update product route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -23,3 +23,33 @@ exports.getAllProducts = async(req, res) => {
         products,
     });
 };
+
+//Update a Product
+exports.updateProducts = async(req, res, next) => {
+
+    let product = await Product.findById(req.params.id);
+
+    if(!product) {
+
+        return res.status(500).json({
+
+            success: false,
+            message: "Product Not Found",
+        })
+    }
+    else {
+
+        product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+
+            new: true,
+            runValidators: true,
+            useFindandModify: false,
+        })
+
+        return res.status(200).json({
+
+            success: true,
+            product,
+        })
+    }
+}
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const {getAllProducts, createProduct} = require("../controllers/productController.js");
+const {getAllProducts, createProduct, updateProducts} = require("../controllers/productController.js");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route("/product/new").post(createProduct);
+router.route("/product/:id").put(updateProducts);
 
 module.exports = router;
