b562cc8a9009155e132041977ff3a7514d4db4ee
Zodeporas06 <zodeporas06@gmail.com>
2023-03-02 15:07:24 +0530

    error handler

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const errorMiddleware = require("./middleware/error");
 
 const app = express();
 
 //To get the data in json file for the database
 app.use(express.json());
 
+//Importing Routes
 const product = require("./routes/productRoute.js");
 
 app.use("/api/v1", product);
+
+//Middleware for Errors
+app.use(errorMiddleware);
 
 module.exports = app;
