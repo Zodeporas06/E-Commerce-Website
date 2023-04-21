df5d345fe8f0a08992cd764da62adfe98f3fd57b
Zodeporas06 <zodeporas06@gmail.com>
2023-04-21 17:47:29 +0530

    Unhandled Promise Rejection

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,22 @@ const app = require("./app.js");
 const dotenv = require("dotenv");
 const connectDatabase = require("./configurations/database.js");
 
-dotenv.config({path:"backend/configurations/files.env"});
+dotenv.config({ path: "backend/configurations/files.env" });
 
 connectDatabase();
 
 const port = process.env.PORT;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
+  console.log("Server is working on http://localhost:" + port);
+});
 
-    console.log("Server is working on http://localhost:" + port);
-})
+//Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
