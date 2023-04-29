ad7837c4a99d10d5892aa6867e31806c5c5959c4
Zodeporas06 <zodeporas06@gmail.com>
2023-04-29 20:17:24 +0530

    Password Encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
 
@@ -51,5 +52,17 @@ const userSchema = new mongoose.Schema({
     resetPasswordExpire: Date,
 
 });
+
+userSchema.pre("save", async function(next){
+
+    if(!this.isModified("password")) {
+
+        next();
+    }
+    else {
+
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+})
 
 module.exports = mongoose.model("User", userSchema);
