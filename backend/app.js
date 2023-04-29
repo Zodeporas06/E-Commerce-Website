c92da1b4142b1548768d3a63c8aa365baaab8075
Zodeporas06 <zodeporas06@gmail.com>
2023-04-29 19:54:41 +0530

    Created User Route

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,10 @@ app.use(express.json());
 
 //Importing Routes
 const product = require("./routes/productRoute.js");
+const user = require("./routes/userRoutes.js");
 
 app.use("/api/v1", product);
+app.use("/api/v1", user);
 
 //Middleware for Errors
 app.use(errorMiddleware);
