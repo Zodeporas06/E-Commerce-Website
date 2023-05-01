3bc1cc583fd731c04ed8cbc3234fb37cda4a787a
Zodeporas06 <zodeporas06@gmail.com>
2023-05-01 20:24:54 +0530

    User Logout

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,3 +48,18 @@ exports.loginUser = catchAsyncErrors(async(req, res, next) => {
 
     sendToken(user, 200, res);
 })
+
+exports.logoutUser = catchAsyncErrors(async(req, res, next) => {
+
+    res.cookie("token", null, {
+
+        httpOnly: true,
+        expires: new Date(Date.now()),
+    });
+
+    res.status(200).json({
+
+        success: true,
+        message: "Logged Out",
+    })
+})
