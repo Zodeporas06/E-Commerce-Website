1b96dd487c6113c48cdcce2631178d7449023572
Zodeporas06 <zodeporas06@gmail.com>
2023-05-01 20:56:41 +0530

    Authentication Roles

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,12 +1,22 @@
 const express = require("express");
-const {getAllProducts, createProduct, updateProducts, deleteProduct, getProductDetails} = require("../controllers/productController.js");
-const { isAuthenticatedUser } = require("../middleware/auth.js");
+const {
+  getAllProducts,
+  createProduct,
+  updateProducts,
+  deleteProduct,
+  getProductDetails,
+} = require("../controllers/productController.js");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth.js");
 
 const router = express.Router();
 
 //Product Routes
-router.route("/products").get(isAuthenticatedUser, getAllProducts);
-router.route("/product/new").post(createProduct);
-router.route("/product/:id").put(updateProducts).delete(deleteProduct).get(getProductDetails);
+router.route("/products").get(getAllProducts);
+router.route("/product/new").post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+router
+  .route("/product/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProducts)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct)
+  .get(getProductDetails);
 
 module.exports = router;
