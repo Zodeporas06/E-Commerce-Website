1dc5a4ad6561d4574c2566dd1ea1dc824236bcc8
Zodeporas06 <zodeporas06@gmail.com>
2023-05-06 22:36:28 +0530

    Get Single User

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {registerUser, loginUser, logoutUser, forgotPassword, resetPassword} = require("../controllers/userController.js");
+const {registerUser, loginUser, logoutUser, forgotPassword, resetPassword, getUserDetails} = require("../controllers/userController.js");
 
 const router = express.Router();
 
@@ -8,5 +8,7 @@ router.route("/login").post(loginUser);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 router.route("/logout").get(logoutUser);
+
+router.route("/me").get(getUserDetails);
 
 module.exports = router;
